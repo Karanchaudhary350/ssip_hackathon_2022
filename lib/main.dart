d1d4d00ac2ca47acb8200d73ad8c712bbe25717d
karan chaudhary <78443850+Karanchaudhary350@users.noreply.github.com>
2022-10-06 09:05:00 +0530

    Revert "Dev"

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
-import 'dart:developer';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:ssip_hackathon_2022/Cases_pages/add_case_page.dart';
-import 'package:ssip_hackathon_2022/Cases_pages/past_cases.dart';
-import 'package:ssip_hackathon_2022/Cases_pages/current_cases.dart';
-import 'package:ssip_hackathon_2022/ani_care_page.dart';
 import 'package:ssip_hackathon_2022/firebase_options.dart';
 
-import 'Authentication/viaEmail/SignupPage.dart';
 import 'Authentication/viaEmail/loginPage.dart';
 import 'home_page.dart';
 
@@ -29,16 +21,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const AniCarePage(),
-      initialRoute: "/anti_care",
+      home: const MyHomePage(),
+      initialRoute: "login",
       routes: {
-        "/home": (context) => const MyHomePage(),
-        "/login": (context) => const LoginScreen(),
-        "/signup": (context) => const RegistrationState(),
-        "/anti_care": (context) => const AniCarePage(),
-        "/past_cases": (context) => const PastCasesPage(),
-        "/current_cases": (context) => const CurrentCasesPage(),
-        "/add_case": (context) => const AddCasePage(),
+        "home": (context) => const MyHomePage(),
+        "login": (context) => const LoginScreen(),
       },
     );
   }
